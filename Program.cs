eedbc4c91e3e9cee69b432f7c0c521d1e7497d44
badjateamisha <99854337+badjateamisha@users.noreply.github.com>
2022-04-27 08:10:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using LogicalProgramming;
+
+
+Console.WriteLine("Basic programming");
+
+
+while (true)
+{
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine("\nChoose the Program to be executed :\n1)Fibonacci Series\n2)Perfect NUmber\n3)Prime Number\n4)Reverse Number\n5)Coupon Number\n6)Stopwatch" + "");    
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            Fibonacciseries fibonacci = new();
+            fibonacci.fibonacciseries();
+            break;
+
+        case 2:
+            PerfectNumber perfect = new();
+            perfect.perfefctnumber();
+            break;
+
+        case 3:
+            PrimeNumber prime = new();
+            prime.primenumber();
+            break;
+
+        case 4:
+            ReverseNumber reverse = new();
+            reverse.reverseNumber();
+            break;
+
+        case 5:
+            CouponNumber coupon = new();
+            coupon.CouponCode();
+            break;
+
+        case 6:
+            stopwatch watch = new();
+            watch.StopWatch();
+            break;
+
+        default:
+            break;
+    }
+}
+
